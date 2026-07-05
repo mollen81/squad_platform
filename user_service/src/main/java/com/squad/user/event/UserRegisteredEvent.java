b463fcf31be1b7bd7163b1ed22bdb460ef30fe15
mollen <stepanpanenko@gmail.com>
2026-07-05 22:04:32 +0300

    user_service Kafka user registration event

---
+++ user_service/src/main/java/com/squad/user/event/UserRegisteredEvent.java
@@ -0,0 +1,18 @@
+package com.squad.user.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRegisteredEvent {
+    private UUID userId;
+    private String steamId;
+    private long timestamp;
+}
---
+++ user_service/src/main/java/com/squad/user/kafka/UserEventProducer.java
@@ -0,0 +1,21 @@
+package com.squad.user.kafka;
+
+import com.squad.user.event.UserRegisteredEvent;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@Slf4j
+@Builder
+@RequiredArgsConstructor
+public class UserEventProducer {
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    private static final String TOPIC_USER_REGISTERED = "user.registered";
+
+    public void sendUserRegisteredEvent(UserRegisteredEvent event) {
+        log.info("Sending an event to Kafka [{}] for the user: {}", TOPIC_USER_REGISTERED, event.getUserId());
+        kafkaTemplate.send(TOPIC_USER_REGISTERED, event.getUserId().toString(), event);
+    }
+}
