dafd3cb721660230bc330fcdbd2a465660f65951
mollen <stepanpanenko@gmail.com>
2026-07-08 11:39:12 +0300

    gateway: AuthGrpcClientService

---
+++ gateway/src/main/java/com/squad/service/AuthGrpcClientService.java
@@ -0,0 +1,40 @@
+package com.squad.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.squad.grpc.user.AuthServiceGrpc;
+import com.squad.grpc.user.ResolveSteamAuthRequest;
+import com.squad.grpc.user.ResolveSteamAuthResponse;
+import lombok.RequiredArgsConstructor;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class AuthGrpcClientService {
+
+    @GrpcClient("user_service")
+    private AuthServiceGrpc.AuthServiceBlockingStub authServiceStub;
+
+    private final ObjectMapper mapper;
+
+    public ResolveSteamAuthResponse loginWithSteam(String openidParamsJson) {
+        try {
+            Map<String, String> params = mapper.readValue(
+                    openidParamsJson,
+                    new TypeReference<>() {});
+            ResolveSteamAuthRequest request = ResolveSteamAuthRequest.newBuilder()
+                    .putAllOpenidParams(params)
+                    .build();
+            return authServiceStub.resolveSteamAuth(request);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(
+                    "Steam authorization request could not be processed: " + e.getMessage());
+        }
+    }
+}
