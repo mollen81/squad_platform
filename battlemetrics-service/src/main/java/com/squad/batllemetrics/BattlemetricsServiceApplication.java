33fd0f00026c915ba0a85b9971e9eed7475622f4
mollen <stepanpanenko@gmail.com>
2026-07-11 13:02:54 +0300

    battlemetrics-service: successfully start

---
+++ battlemetrics-service/src/main/java/com/squad/batllemetrics/BattlemetricsServiceApplication.java
@@ -0,0 +1,11 @@
+package com.squad.batllemetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattlemetricsServiceApplication {
+    public static void main(String[] args){
+        SpringApplication.run(BattlemetricsServiceApplication.class, args);
+    }
+}
