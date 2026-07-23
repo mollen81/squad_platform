a0ef21434f291b941b12f39e16e886c989f599c2
mollen <stepanpanenko@gmail.com>
2026-07-23 15:52:14 +0300

    clan-service: gRPC fetching fix && StatsGrpcClient

--- clan-service/src/main/java/com/squad/clan/client/StatsGrpcClient.java
+++ clan-service/src/main/java/com/squad/clan/client/StatsGrpcClient.java
@@ -1,9 +1,14 @@
 package com.squad.clan.client;
 
+import com.squad.grpc.stats.GetPlayerStatsByInternalIdRequest;
+import com.squad.grpc.stats.GetPlayerStatsResponse;
+import com.squad.grpc.stats.StatsServiceGrpc;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @Slf4j
@@ -11,5 +16,21 @@ import org.springframework.stereotype.Service;
 public class StatsGrpcClient {
 
     @GrpcClient("stats-service")
-    private StatsServiceGrpc.
+    private StatsServiceGrpc.StatsServiceBlockingStub statsServiceStub;
+
+    public int getPlayerElo(UUID userId) {
+        try {
+            GetPlayerStatsByInternalIdRequest request = GetPlayerStatsByInternalIdRequest.newBuilder()
+                    .setUserId(userId.toString())
+                    .build();
+
+            GetPlayerStatsResponse response = statsServiceStub.getPlayerStats(request);
+            return response.getEloRating();
+        }
+        catch (Exception e) {
+            log.warn("Couldn't get ELO for player {}. We use the basic 1000 ELO. Reason: {}", userId, e.getMessage());
+            return 1000;
+        }
+    }
+
 }
