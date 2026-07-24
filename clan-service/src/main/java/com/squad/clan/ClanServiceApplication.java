16dfafa2b7046064c231efb8ec3005eaa8717d0f
mollen <stepanpanenko@gmail.com>
2026-07-24 11:56:52 +0300

    clan-service: ClanServiceApplication main executable class

---
+++ clan-service/src/main/java/com/squad/clan/ClanServiceApplication.java
@@ -0,0 +1,11 @@
+package com.squad.clan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClanServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClanServiceApplication.class, args);
+    }
+}
