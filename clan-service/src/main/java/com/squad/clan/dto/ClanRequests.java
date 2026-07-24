63b158fd4ebb44a10503436ff1f43f90b49bc60d
mollen <stepanpanenko@gmail.com>
2026-07-24 10:46:42 +0300

    clan-service: Clan & ClanRequests data schema update

--- clan-service/src/main/java/com/squad/clan/dto/ClanRequests.java
+++ clan-service/src/main/java/com/squad/clan/dto/ClanRequests.java
@@ -1,5 +1,8 @@
 package com.squad.clan.dto;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 import java.util.UUID;
@@ -18,6 +21,9 @@ public class ClanRequests {
 
     @Data
     public static class ApplyToClanDto {
+        @Id
+        @GeneratedValue(strategy = GenerationType.UUID)
+        private UUID id; // application id
         private UUID userId; // В реальном проекте берется из JWT токена
         private UUID clanId;
         private String socialLink;
