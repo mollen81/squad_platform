0574a5906e961b1ae5e4039794e48be0a3ccbb3d
mollen <stepanpanenko@gmail.com>
2026-07-28 21:56:54 +0300

    clan-service: The risk of an N+1 query problem in clan-service issue fixed

--- clan-service/src/main/java/com/squad/clan/repository/ClanRepository.java
+++ clan-service/src/main/java/com/squad/clan/repository/ClanRepository.java
@@ -1,13 +1,12 @@
 package com.squad.clan.repository;
 
 import com.squad.clan.entity.Clan;
-import com.squad.clan.entity.ClanMember;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -18,4 +17,7 @@ public interface ClanRepository extends JpaRepository<Clan, UUID> {
 
     @Query("SELECT c FROM Clan c LEFT JOIN FETCH c.members WHERE c.id = :clanId")
     Optional<Clan> findByIdWithAllMembers(@Param("clanId") UUID clanId);
+
+    @EntityGraph(attributePaths = {"members"})
+    Optional<Clan> findWithMembersById(UUID id);
 }
