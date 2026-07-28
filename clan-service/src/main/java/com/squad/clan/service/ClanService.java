0e02ea2633a25625f969c375be1afd0d1996efab
mollen <stepanpanenko@gmail.com>
2026-07-28 11:30:24 +0300

    clan-service: process acceptance method for ClanService

--- clan-service/src/main/java/com/squad/clan/service/ClanService.java
+++ clan-service/src/main/java/com/squad/clan/service/ClanService.java
@@ -15,6 +15,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -67,17 +69,17 @@ public class ClanService {
 
         if(clanMemberRepository.existsByUserId(dto.getUserId())) {
             String clanName = clanRepository.getReferenceById(dto.getClanId()).getName();
-            throw new IllegalStateException("You're already participates the clan: " +
-                    clanName + ".You need to leave current clan.");
+            throw new IllegalStateException("User is already participates the clan: " +
+                    clanName + ". He needs to leave current clan.");
         }
 
         if(clan.getMinElo() > userElo) {
-            throw new IllegalStateException("Your ELO is lower, than minimal required ELO for this clan");
+            throw new IllegalStateException("User's ELO is lower, than minimal required ELO for this clan");
         }
 
         if(clanApplicationRepository.existsByUserIdAndClanIdAndStatus(
                         dto.getUserId(), dto.getClanId(), ApplicationStatus.PENDING)) {
-            throw new IllegalStateException("Your application is already being reviewed by the clan");
+            throw new IllegalStateException("User's application is already being reviewed by the clan");
         }
 
         if(!clan.getIsRecruiting()) {
@@ -94,7 +96,7 @@ public class ClanService {
 
         application = clanApplicationRepository.save(application);
 
-        log.info("Player {} is sent application to clan {}",
+        log.info("User {} is sent application to clan {}",
                 application.getUserId(), application.getClan().getName());
         return application;
     }
@@ -107,5 +109,62 @@ public class ClanService {
 
         log.info("Clan: {} is successfully fetched", clan.getName());
         return clan;
+    }
+
+
+    @Transactional
+    public ClanMember processAcceptance(ClanRequests.AcceptApplicationDto dto, int applicantElo) {
+        ClanApplication clanApplication = clanApplicationRepository.findById(dto.applicationId())
+                .orElseThrow(() -> new IllegalArgumentException("Application not found"));
+
+        if(clanApplication.getStatus() != ApplicationStatus.PENDING) {
+            throw new IllegalStateException("Application is already processed");
+        }
+
+        Clan clan = clanRepository.findById(clanApplication.getClan().getId())
+                .orElseThrow(() -> new IllegalArgumentException("Clan not found"));
+
+        ClanMember acceptor = clanMemberRepository.findByUserId(dto.accepterId())
+                .orElseThrow(() -> new IllegalArgumentException("Acceptor not found"));
+
+        if(!acceptor.getClan().getId().equals(clan.getId()) ||
+                (acceptor.getRole().equals(ClanRole.LEADER) || (acceptor.getRole().equals(ClanRole.MODERATOR)))) {
+            throw new IllegalStateException(
+                    "Acceptor don't have correct rights: he isn't participates traget clan or do not LEADER or MODERATOR of target clan"
+            );
+        }
+
+        if(applicantElo < clan.getMinElo()) {
+            throw new IllegalStateException("User's ELO is lower than minimal clan ELO");
+        }
+
+        if(clanApplicationRepository.existsById(dto.applicationId())) {
+            clanApplication.setStatus(ApplicationStatus.REJECTED);
+            clanApplicationRepository.save(clanApplication);
+            throw new IllegalStateException("User is already sent application to another clan");
+        }
+
+        clanApplication.setStatus(ApplicationStatus.APPROVED);
+        clanApplicationRepository.save(clanApplication);
+
+        List<ClanApplication> otherPendingApplications = clanApplicationRepository.findByUserIdAndStatus(clanApplication.getUserId(), ApplicationStatus.PENDING);
+        for(ClanApplication application : otherPendingApplications) {
+            application.setStatus(ApplicationStatus.REJECTED);
+        }
+        clanApplicationRepository.saveAll(otherPendingApplications);
+
+        ClanMember newMember = ClanMember.builder()
+                .userId(clanApplication.getUserId())
+                .clan(clan)
+                .role(ClanRole.MEMBER)
+                .build();
+        newMember = clanMemberRepository.save(newMember);
+
+        clan.setTotalElo(clan.getTotalElo() + applicantElo);
+        clan.getMembers().add(newMember);
+
+        log.info("Application: {} accepted. User: {} joined clan: {}. New total ELO: {}",
+                clanApplication.getId(), newMember.getUserId(), clan.getName(), clan.getTotalElo());
+        return newMember;
     }
 }
