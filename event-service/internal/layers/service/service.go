98e1dfb073a177586c3c96fea7507a5060acd4e6
Lozovoy Stepan <lskuku1211@gmail.com>
2026-08-03 17:22:10 +0300

    kafka publish in service layer

--- event-service/internal/layers/service/service.go
+++ event-service/internal/layers/service/service.go
@@ -6,17 +6,20 @@ import (
 	"errors"
 
 	domain "event-service/internal/core/domain"
+	kafka "event-service/internal/core/kafka"
 
 	uuid "github.com/google/uuid"
 )
 
 type eventService struct {
 	eventRepo EventRepository
+	producer  *kafka.Producer
 }
 
-func NewEventService(eventRepo EventRepository) EventService {
+func NewEventService(eventRepo EventRepository, producer *kafka.Producer) EventService {
 	return &eventService{
 		eventRepo: eventRepo,
+		producer:  producer,
 	}
 }
 
@@ -31,139 +34,137 @@ func (s *eventService) CreateEvent(ctx context.Context, userCreateID, eventName 
 	}
 
 	if err := s.eventRepo.CreateEvent(ctx, event); err != nil {
-		/*kafka*/
 		return err
 	}
 
 	if err := s.eventRepo.JoinToEvent(ctx, userCreateID, event.EventID, time.Now()); err != nil {
-		/*kafka*/
 		return err
 	}
 
-	/*kafka*/
+	if err := s.producer.PublishEventCreated(ctx, event); err != nil {
+		return err
+	}
+
+	if err := s.producer.PublishUserJoinedEvent(ctx, event.EventID, userCreateID, time.Now()); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (s *eventService) GetEventsByUserID(ctx context.Context, userCreateID string) ([]*domain.Event, error) {
 	events, err := s.eventRepo.GetEventsByUserID(ctx, userCreateID)
 	if err != nil {
-		/*kafka*/
 		return nil, err
 	}
 
-	/*kafka*/
 	return events, nil
 }
 
 func (s *eventService) GetEventsByEventName(ctx context.Context, eventName string) ([]domain.Event, error) {
 	event, err := s.eventRepo.GetEventsByEventName(ctx, eventName)
 	if err != nil {
-		/*kafka*/
 		return []domain.Event{}, err
 	}
 
-	/*kafka*/
 	return event, nil
 }
 
 func (s *eventService) UpdateTimeEvent(ctx context.Context, eventID, userCreateID string, newTimeStart time.Time) error {
 	event, err := s.eventRepo.GetEventByID(ctx, eventID)
 	if err != nil {
-		/*kafka*/
 		return err
 	}
 
 	if event.UserCreateID != userCreateID {
-		/*kafka*/
 		return errors.New("you are not admin")
 	}
 
 	if err = s.eventRepo.UpdateTimeEvent(ctx, eventID, newTimeStart); err != nil {
-		/*kafka*/
 		return err
 	}
 
-	/*kafka*/
+	if err = s.producer.PublishEventTimeUpdated(ctx, eventID, userCreateID, newTimeStart); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (s *eventService) DeleteEvent(ctx context.Context, eventID, userCreateID string) error {
 	event, err := s.eventRepo.GetEventByID(ctx, eventID)
 	if err != nil {
-		/*kafka*/
 		return err
 	}
 
 	if event.UserCreateID != userCreateID {
-		/*kafka*/
 		return errors.New("you are not admin")
 	}
 
 	if err = s.eventRepo.DeleteEvent(ctx, eventID); err != nil {
-		/*kafka*/
 		return err
 	}
 
-	/*kafka*/
+	if err = s.producer.PublishEventDeleted(ctx, eventID, userCreateID); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (s *eventService) JoinToEvent(ctx context.Context, eventID, userID string, joinTime time.Time) error {
 	event, err := s.eventRepo.GetEventByID(ctx, eventID)
 	if err != nil {
-		/*kafka*/
 		return err
 	}
 
 	if event.EventID == "" {
-		/*kafka*/
 		return errors.New("event not found")
 	}
 
 	if err = s.eventRepo.JoinToEvent(ctx, userID, eventID, joinTime); err != nil {
-		/*kafka*/
 		return err
 	}
 
-	/*kafka*/
+	if err = s.producer.PublishUserJoinedEvent(ctx, eventID, userID, joinTime); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (s *eventService) LeaveEvent(ctx context.Context, userID, eventID string) error {
 	event, err := s.eventRepo.GetEventByID(ctx, eventID)
 	if err != nil {
-		/*kafka*/
 		return err
 	}
 
 	if event.EventID == "" {
-		/*kafka*/
 		return errors.New("event not found")
 	}
 
 	if event.UserCreateID == userID {
-		/*kafka*/
 		return errors.New("admin cannot leave event, use DeleteEvent instead")
 	}
 
 	if err = s.eventRepo.LeaveEvent(ctx, userID, eventID); err != nil {
-		/*kafka*/
 		return err
 	}
 
-	/*kafka*/
+	if err = s.producer.PublishUserLeftEvent(ctx, eventID, userID); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (s *eventService) CreateGame(ctx context.Context, eventID, mapID string, timeStart time.Time) error {
 	event, err := s.eventRepo.GetEventByID(ctx, eventID)
 	if err != nil {
-		/*kafka*/
 		return err
 	}
 
 	if event.EventID == "" {
-		/*kafka*/
 		return errors.New("event not found")
 	}
 
@@ -178,145 +179,138 @@ func (s *eventService) CreateGame(ctx context.Context, eventID, mapID string, ti
 	}
 
 	if err := s.eventRepo.CreateGame(ctx, game); err != nil {
-		/*kafka*/
 		return err
 	}
 
-	/*kafka*/
+	if err := s.producer.PublishGameCreated(ctx, game); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (s *eventService) GetGameByID(ctx context.Context, gameID string) (domain.Game, error) {
 	game, err := s.eventRepo.GetGameByID(ctx, gameID)
 	if err != nil {
-		/*kafka*/
 		return domain.Game{}, err
 	}
 
 	if game.GameID == "" {
-		/*kafka*/
 		return domain.Game{}, errors.New("game not found")
 	}
 
-	/*kafka*/
 	return game, nil
 }
 
 func (s *eventService) GetGamesByEventID(ctx context.Context, eventID string) ([]domain.Game, error) {
 	games, err := s.eventRepo.GetGamesByEventID(ctx, eventID)
 	if err != nil {
-		/*kafka*/
 		return nil, err
 	}
 
-	/*kafka*/
 	return games, nil
 }
 
 func (s *eventService) UpdateGameWinner(ctx context.Context, gameID, winnerTeamID string) error {
 	game, err := s.eventRepo.GetGameByID(ctx, gameID)
 	if err != nil {
-		/*kafka*/
 		return err
 	}
 
 	if game.GameID == "" {
-		/*kafka*/
 		return errors.New("game not found")
 	}
 
 	if err = s.eventRepo.UpdateGameWinner(ctx, gameID, winnerTeamID); err != nil {
-		/*kafka*/
 		return err
 	}
 
-	/*kafka*/
+	if err = s.producer.PublishGameWinnerUpdated(ctx, gameID, winnerTeamID); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (s *eventService) UpdateGameLoser(ctx context.Context, gameID, loserTeamID string) error {
 	game, err := s.eventRepo.GetGameByID(ctx, gameID)
 	if err != nil {
-		/*kafka*/
 		return err
 	}
 
 	if game.GameID == "" {
-		/*kafka*/
 		return errors.New("game not found")
 	}
 
 	if err = s.eventRepo.UpdateGameLoser(ctx, gameID, loserTeamID); err != nil {
-		/*kafka*/
 		return err
 	}
 
-	/*kafka*/
+	if err = s.producer.PublishGameLoserUpdated(ctx, gameID, loserTeamID); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (s *eventService) FinishGame(ctx context.Context, gameID string, timeFinish time.Time) error {
 	game, err := s.eventRepo.GetGameByID(ctx, gameID)
 	if err != nil {
-		/*kafka*/
 		return err
 	}
 
 	if game.GameID == "" {
-		/*kafka*/
 		return errors.New("game not found")
 	}
 
 	if err = s.eventRepo.FinishGame(ctx, gameID, timeFinish); err != nil {
-		/*kafka*/
 		return err
 	}
 
-	/*kafka*/
+	if err = s.producer.PublishGameFinished(ctx, gameID, timeFinish); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (s *eventService) DeleteGame(ctx context.Context, gameID string) error {
 	game, err := s.eventRepo.GetGameByID(ctx, gameID)
 	if err != nil {
-		/*kafka*/
 		return err
 	}
 
 	if game.GameID == "" {
-		/*kafka*/
 		return errors.New("game not found")
 	}
 
 	if err = s.eventRepo.DeleteGame(ctx, gameID); err != nil {
-		/*kafka*/
 		return err
 	}
 
-	/*kafka*/
+	if err = s.producer.PublishGameDeleted(ctx, gameID); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (s *eventService) AddUserStatsToGame(ctx context.Context, gameID, userID string, kills, deaths, points int64) error {
 	game, err := s.eventRepo.GetGameByID(ctx, gameID)
 	if err != nil {
-		/*kafka*/
 		return err
 	}
 
 	if game.GameID == "" {
-		/*kafka*/
 		return errors.New("game not found")
 	}
 
 	user, err := s.eventRepo.GetUserByID(ctx, userID)
 	if err != nil {
-		/*kafka*/
 		return err
 	}
 
 	if user.UserID == "" {
-		/*kafka*/
 		return errors.New("user not found")
 	}
 
@@ -330,33 +324,30 @@ func (s *eventService) AddUserStatsToGame(ctx context.Context, gameID, userID st
 	}
 
 	if err := s.eventRepo.AddUserStatsToGame(ctx, stats); err != nil {
-		/*kafka*/
 		return err
 	}
 
-	/*kafka*/
+	if err := s.producer.PublishUserStatsAdded(ctx, stats); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func (s *eventService) GetGameStats(ctx context.Context, gameID string) ([]domain.GameUserStats, error) {
 	game, err := s.eventRepo.GetGameByID(ctx, gameID)
 	if err != nil {
-		/*kafka*/
 		return nil, err
 	}
 
 	if game.GameID == "" {
-		/*kafka*/
 		return nil, errors.New("game not found")
 	}
 
 	stats, err := s.eventRepo.GetGameStats(ctx, gameID)
 	if err != nil {
-		/*kafka*/
 		return nil, err
 	}
 
-	/*kafka*/
 	return stats, nil
 }
-
