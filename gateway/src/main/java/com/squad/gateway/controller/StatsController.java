613b1b1b6b58a3e3d39cfc9f35361e1a7a89ca20
mollen <stepanpanenko@gmail.com>
2026-09-04 16:04:46 +0300

    gateway: StatsController -> REST StatsController

--- gateway/src/main/java/com/squad/gateway/controller/StatsController.java
+++ gateway/src/main/java/com/squad/gateway/controller/StatsController.java
@@ -1,30 +1,47 @@
 package com.squad.gateway.controller;
 
-import com.squad.gateway.record.StatsService.PlayerStats;
 import com.squad.gateway.service.StatsGrpcClientService;
 import com.squad.stats.grpc.GetPlayerStatsResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.graphql.data.method.annotation.Argument;
-import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 public class StatsController {
     private final StatsGrpcClientService statsGrpcClientService;
 
-    @QueryMapping
-    public PlayerStats getPlayerStats(@Argument String userId) {
-        GetPlayerStatsResponse grpcResponse = statsGrpcClientService.getPlayerStats(userId);
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getPlayerStats(@PathVariable String userId) {
+        try {
+            GetPlayerStatsResponse grpcResponse = statsGrpcClientService.getPlayerStats(userId);
 
-        return new PlayerStats(
-                grpcResponse.getEloRating(),
-                grpcResponse.getTotalPlaytimeHours(),
-                grpcResponse.getKills(),
-                grpcResponse.getVehiclesDestroyed(),
-                grpcResponse.getDeaths(),
-                grpcResponse.getRevives(),
-                grpcResponse.getFavouriteRole()
-        );
+            StatsResponse response = new StatsResponse(
+                    grpcResponse.getEloRating(),
+                    grpcResponse.getKills(),
+                    grpcResponse.getDeaths(),
+                    grpcResponse.getRevives(),
+                    grpcResponse.getFavouriteRole(),
+                    grpcResponse.getTotalPlaytimeHours()
+            );
+            return ResponseEntity.ok(response);
+        }
+        catch (Exception e) {
+            return ResponseEntity.status(404).body(Map.of("error", "Stats is not found: " + e.getMessage()));
+        }
     }
+
+
+    public record StatsResponse(
+            int eloRating,
+            int kills,
+            int deaths,
+            int revives,
+            String favouriteRole,
+            int totalPlaytimeHours
+    ) {}
 }
