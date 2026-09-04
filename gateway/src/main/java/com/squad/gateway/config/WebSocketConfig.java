2e69545694fbe424af0837a7169fac51d4927d48
mollen <stepanpanenko@gmail.com>
2026-09-04 15:55:52 +0300

    gateway: WebSocketConfig

--- gateway/src/main/java/com/squad/gateway/config/WebSocketConfig.java
+++ gateway/src/main/java/com/squad/gateway/config/WebSocketConfig.java
@@ -4,11 +4,19 @@ import com.squad.gateway.websocket.NotificationHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 @EnableWebSocket
 @RequiredArgsConstructor
-public class WebSocketConfig {
+public class WebSocketConfig implements WebSocketConfigurer {
 
     private final NotificationHandler notificationHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(notificationHandler, "/ws/notifications")
+                .setAllowedOrigins("https://kolibrisquad.ru");
+    }
 }
