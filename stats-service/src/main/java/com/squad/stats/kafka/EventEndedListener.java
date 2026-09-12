1a4fe5da4bb1142adfbaecc904eeb0a8b939db95
mollen <stepanpanenko@gmail.com>
2026-09-12 21:25:45 +0300

    stats-service: EventEndedListener kafka listener for event end processing

--- stats-service/src/main/java/com/squad/stats/kafka/EventEndedListener.java
+++ stats-service/src/main/java/com/squad/stats/kafka/EventEndedListener.java
@@ -1,7 +1,7 @@
 package com.squad.stats.kafka;
 
 import com.squad.stats.dto.EventEndedEvent;
-import com.squad.stats.service.EventGrpcClient;
+import com.squad.stats.service.EventResultsProcessingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -12,10 +12,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EventEndedListener {
 
-    EventGrpcClient eventGrpcClient;
+    EventResultsProcessingService processingService;
 
-    @KafkaListener(topics = "event.ended")
+    @KafkaListener(topics = "event.ended", groupId = "stats-service-group")
     public void onEventEnded(EventEndedEvent event) {
-
+        log.info("Event {} ended. Starting to process results and update users stats.", event.getEventId());
+        processingService.processEventResults(event.getEventId());
     }
 }
