8645a966ff74fdcb9657893dab5c98fcc6422147
mollen <stepanpanenko@gmail.com>
2026-09-24 13:02:31 +0300

    deployment-service: basic changes

---
+++ deployment-service/src/main/java/com/squad/deployment/DeploymentApplication.java
@@ -0,0 +1,11 @@
+package com.squad.deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DeploymentApplication.class, args);
+    }
+}
